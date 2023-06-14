fb0bd5144b5c3e30c699f00734d78b93e695bcf7
Ignacio Smirlian <ignacio.smirlian@globant.com>
2023-06-14 15:59:10 -0300

    upload files OK. Details missing

--- src/components/pure/FormAddGoal.jsx
+++ src/components/pure/FormAddGoal.jsx
@@ -1,11 +1,14 @@
 import { Form, Formik } from "formik";
 import * as yup from "yup";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import CustomInput from "./CustomInput";
 import { CircularProgress } from "@mui/material";
 import "./FormAddGoal.css";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import uploadIcon from "../../assets/images/upload-icon.png";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { app } from "../../firebase/fibaseConfig";
 
 const goalSchema = yup.object().shape({
   description: yup
@@ -23,17 +26,54 @@ const FormAddGoal = () => {
     description: "",
     total: 0,
     currency: "pesos",
+    image: "",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [selectedImage, setSelectedImage] = useState({ url: "", file: "" });
   const location = useLocation();
+  const inputRef = useRef(null);
+  const storage = getStorage(app);
+
+  useEffect(() => {
+    //console.log(goalToSubmit);
+  }, [goalToSubmit]);
+
+  useEffect(() => {
+    console.log(selectedImage);
+  }, [selectedImage]);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const imageRegex = /\.(jpe?g|png|gif|bmp|ico|svg)$/i;
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj || !imageRegex.test(fileObj.name)) {
+      return;
+    } else {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setSelectedImage({ ...selectedImage, file: reader.result });
+      };
+      reader.readAsDataURL(fileObj);
+      setSelectedImage({ ...selectedImage, url: event.target.files[0] });
+    }
+  };
 
   const handleChange = (field, value) => {
     setGoalToSubmit({ ...goalToSubmit, [field]: value });
   };
 
-  useEffect(() => {
-    console.log(goalToSubmit);
-  }, [goalToSubmit]);
+  const updloadFile = () => {
+    const storageRef = ref(
+      storage,
+      `${location.state.userUID}/${goalToSubmit.description}`
+    );
+    uploadBytes(storageRef, selectedImage.url).then((snapshot) => {
+      console.log(snapshot);
+    });
+  };
 
   const handleGoalSubmit = async (values, { resetForm }) => {
     setIsSubmitting(true);
@@ -92,6 +132,33 @@ const FormAddGoal = () => {
                 <option value={"Dolares"}>Dolares</option>
               </select>
             </div>
+            <div className="goal-image-container">
+              <label>
+                {goalToSubmit.image === "" ? (
+                  "Sin imagen"
+                ) : (
+                  <div className="image-selected-container">
+                    <img src={selectedImage.file} alt="Selected" />
+                  </div>
+                )}
+              </label>
+              <input
+                style={{ display: "none" }}
+                ref={inputRef}
+                type="file"
+                onChange={handleFileChange}
+                accept="image/*"
+              />
+              <button className="upload-button" onClick={handleClick}>
+                Subir imagen
+                <img
+                  className="upload-icon"
+                  src={uploadIcon}
+                  alt="upload"
+                  name="image"
+                ></img>
+              </button>
+            </div>
             <button className="submit-btn" type="submit">
               {isSubmitting ? (
                 <CircularProgress size={20}></CircularProgress>
@@ -102,6 +169,8 @@ const FormAddGoal = () => {
           </Form>
         )}
       </Formik>
+
+      <button onClick={updloadFile}>Test</button>
     </div>
   );
 };
